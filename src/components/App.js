d8f41b527ebbf1560d254a94e3dcb079e5b23aa1
Jun-cloud <kjy8827@gmail.com>
2020-10-20 21:25:25 +0900

    Using firebase auth part two

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+// jsconfig.json의 경로 설정으로 인해 ../ ./ 대신 아래와 같이 import할 수 있게 됨
 import AppRouter from 'components/Router';
-import myFirebase from "myFirebase";
+import { authService } from "myFirebase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // https://firebase.google.com/docs/reference
+  // https://firebase.google.com/docs/reference/js/firebase.auth.Auth
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <AppRouter isLoggedIn={ isLoggedIn }/>
   );
