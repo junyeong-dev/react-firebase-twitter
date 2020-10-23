6f337cd2ad1fe5ea7afdcf0e60bd7a8d61a20b55
Jun-cloud <kjy8827@gmail.com>
2020-10-23 18:23:02 +0900

    Create Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { authService } from "myFirebase";
 /* 
     Auth 관리
     firebase - console - authentication - sign in method (Email, Google, Github 선택; 물론 그 외에 것을 써도 상관없음)
@@ -11,6 +12,7 @@ import React, { useState } from "react";
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if(name === "email"){
@@ -19,7 +21,22 @@ const Auth = () => {
             setPassword(value);
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        // reference : https://firebase.google.com/docs/reference/js/firebase.auth.Auth?hl=ko#createuserwithemailandpassword
+        let data;
+        try {
+            if(newAccount) {
+                // create account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch(error) {
+            console.log(error);
+        }
         
     }
     return(
@@ -27,7 +44,7 @@ const Auth = () => {
             <form onSubmit={ onSubmit }>
                 <input name="email" type="text" placeholder="Email" required value={ email } onChange={ onChange } />
                 <input name="password" type="password" placeholder="Password" required value={ password } onChange={ onChange } />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
