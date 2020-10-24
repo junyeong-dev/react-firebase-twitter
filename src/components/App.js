fc38b02955c10a55c4f1d0e4268ef18af8a30fc8
Jun-cloud <kjy8827@gmail.com>
2020-10-24 18:16:26 +0900

    Create Log in function

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // jsconfig.json의 경로 설정으로 인해 ../ ./ 대신 아래와 같이 import할 수 있게 됨
 import AppRouter from 'components/Router';
 import { authService } from "myFirebase";
@@ -6,9 +6,22 @@ import { authService } from "myFirebase";
 function App() {
   // https://firebase.google.com/docs/reference
   // https://firebase.google.com/docs/reference/js/firebase.auth.Auth
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
   return (
-    <AppRouter isLoggedIn={ isLoggedIn }/>
+    <>
+      { init ? <AppRouter isLoggedIn={ isLoggedIn }/> : "Initializing..." }
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -13,6 +13,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if(name === "email"){
@@ -28,6 +29,7 @@ const Auth = () => {
         try {
             if(newAccount) {
                 // create account
+                // 아이디를 만든 후 firebase console의 Autentication에서 확인 가능
                 data = await authService.createUserWithEmailAndPassword(email, password);
             } else {
                 // log in
@@ -35,17 +37,22 @@ const Auth = () => {
             }
             console.log(data);
         } catch(error) {
-            console.log(error);
+            setError(error.message);
         }
         
     }
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return(
         <div>
             <form onSubmit={ onSubmit }>
                 <input name="email" type="text" placeholder="Email" required value={ email } onChange={ onChange } />
                 <input name="password" type="password" placeholder="Password" required value={ password } onChange={ onChange } />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                { error }
             </form>
+            <span onClick={ toggleAccount }>{ newAccount ? "Sign In" : "Create Account" }</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
