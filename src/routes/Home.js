30a23017428f3a9d4e125071ea63d1ab2d54a210
Jun-cloud <kjy8827@gmail.com>
2020-10-28 20:42:11 +0900

    Create Tweet Form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,21 @@
-import React from"react";
+import React, { useState } from"react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [tweet, setTweet] = useState("");
+    const onSubmit = (event) => {
+        event.perventDefault();
+    }
+    const onChange= (event) => {
+        const { target:{ value }} = event;
+        setTweet(value);
+    }
+    return (
+    <div>
+        <form onSubmit={ onSubmit }>
+            <input type="text" placeholder="what's on your mind" maxLength={ 120 } value={ tweet } onChange={ onChange }/>
+            <input type="submit" value="Tweet"/>
+        </form>
+    </div>
+    );
+}
 export default Home;
