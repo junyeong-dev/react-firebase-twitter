43217bd98220a6df8929a11d84eeb41da3faeb06
Jun-cloud <kjy8827@gmail.com>
2020-10-31 10:43:06 +0900

    Showing tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,14 @@ const Home = () => {
         // reference : https://firebase.google.com/docs/reference/js/firebase.firestore.QuerySnapshot?hl=ko
         // get()의 리턴 값은 QuerySnapshot
         const dvTweets = await dbService.collection("tweets").get();
-        dvTweets.forEach((document) => console.log(document.data()));
+        // dvTweets.forEach((document) => console.log(document.data()));
+        dvTweets.forEach((document) => {
+            const tweetObject = {
+                ...document.data(),
+                id: document.id
+            }
+            setTweets((prev) => [tweetObject, ...prev]);
+        });
     }
     useEffect(() => {
         getTweets();
@@ -34,6 +41,13 @@ const Home = () => {
             <input type="text" placeholder="what's on your mind" maxLength={ 120 } value={ tweet } onChange={ onChange }/>
             <input type="submit" value="Tweet"/>
         </form>
+        <div>
+            { tweets.map(tweet => <div key={ tweet.id }>
+                <h4>
+                    { tweet.tweet }
+                </h4>
+            </div>) }
+        </div>
     </div>
     );
 }
